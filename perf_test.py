57c406dae5865632dbbe173db83ee6908730134d
bakunobu <38971342+bakunobu@users.noreply.github.com>
2019-07-10 16:54:32 +0300

    Update perf_test.py

--- perf_test.py
+++ perf_test.py
@@ -6,11 +6,11 @@ Created on Wed Jul 10 16:45:29 2019
 @author: bakunobu
 """
 
-from sum_of_powered import python_gen_sum, numpy_sum
+from sum_of_powered import python_gen_sum, numpy_sum, python_sum
 from chrono_module import find_duration
 
-a = find_duration(python_gen_sum(5000000))
-b = find_duration(numpy_sum(5000000))
+bare_python = find_duration(python_sum(5000000))
+gen_python = find_duration(python_gen_sum(5000000))
+numpy_python = find_duration(numpy_sum(5000000))
 
-print(a)
-print(b)
+print(f'{bare_python} \n {gen_python} \n {numpy_python}')
