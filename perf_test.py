eb976b3cb9d377bf7b61e24d80f0e54ab10d5816
bakunobu <38971342+bakunobu@users.noreply.github.com>
2019-07-10 22:05:18 +0300

    Update perf_test.py

--- perf_test.py
+++ perf_test.py
@@ -6,11 +6,14 @@ Created on Wed Jul 10 16:45:29 2019
 @author: bakunobu
 """
 
-from sum_of_powered import python_gen_sum, numpy_sum, pythonsum
-from chrono_module import find_duration
+from sum_of_powered import python_gen_sum, numpy_sum, pythonsum 
+from chrono_module import find_durationn
 
-bare_python = find_duration(pythonsum, 10000)
-gen_python = find_duration(python_gen_sum, 10000)
-numpy_python = find_duration(numpy_sum, 10000)
+n = 100000 # number of iterations
+my_funcs = [pythonsum, python_gen_sum, numpy_sum] #list of functions to be tested
 
-print(f'{bare_python} \n {gen_python} \n {numpy_python}')
+
+for func in my_funcs:
+    print(func, find_duration(func, n)[0]) # print an object and time
+    
+    
